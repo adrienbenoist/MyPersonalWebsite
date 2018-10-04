261321a59a4c6e61430619000abd35fd2ed4d6b8
Adrien Benoist <adrien.benoist45@gmail.com>
2018-10-04 06:25:01 +0300

    Changed cv.web.js to render data, wip

--- chunks/intro/screens/cv.web.js
+++ chunks/intro/screens/cv.web.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import { Screen } from 'react-dom-chunky'
+import { Steps } from 'antd'
+import Experience from "../data/experiences"
+import Educations from "../data/educations"
+
+const Step = Steps.Step
 
 export default class CvScreen extends Screen {
   constructor (props) {
     super(props)
     this.state = { ...this.state }
+    this._details = (experience) => this.details.bind(this, experience)
   }
 
   componentDidMount () {
     super.componentDidMount()
   }
 
+  details (experience) {
+    this.triggerRawRedirect(`../data/experiences/${experience.id}`)
+  }
+
+  renderExperiences() {
+
+  }
+
+  renderMainContent() {
+
+  }
+
   components () {
-    return super.components().concat([])
+    return super.components().concat([renderMainContent()])
   }
 }
